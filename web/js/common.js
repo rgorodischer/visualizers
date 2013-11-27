2abf7d854e7a67f9f92b5dbdc33dcb3af87c15aa
Roman Gorodischer <Qwest@Romans-MacBook-Air-2.local>
2013-11-27 23:09:15 +0200

    PoC: Bootstrap UI and TSP visualizer with d3.js

---
+++ web/js/common.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+    _bindNavigationEvents();
+});
+
+(function _configExtraJSFunctions() {
+    String.prototype.startsWith = function(str) {
+        return this.indexOf(str) == 0
+    };
+
+    String.prototype.contains = function(str) {
+        return this.indexOf(str) != -1
+    };
+
+    if (typeof console === "undefined"){
+        console={};
+        console.log = function(){}
+    }
+})();
+
+function _bindNavigationEvents() {
+	$('.navbar a[href="#visualizers"], .navbar a[href="#examples"], .navbar a[href="#contacts"]').click(function() {
+		$('.navbar a').removeClass('selected');
+		$(this).addClass('selected');
+	});
+}
+
+function _loadJsonFile(fileName, callback) {
+	$.getJSON(fileName, function(data) {
+        if (callback) {
+            callback(data)
+        }
+    });
+}
+
+function _applyTemplate(templateName, data) {
+    if (!ich[templateName]) {
+        $.ajax({
+            async: false,
+            cache: false,
+            dataType: "text",
+            url: 'templates/' + templateName + ".tmpl",
+            complete: function(response) {
+                ich.addTemplate(templateName, response.responseText);
+            }
+        });
+        console.log('Loaded ' + templateName + ' template')
+    }
+    return ich[templateName](data);
+}
---
+++ web/js/problems.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+    _bindProblemSetSelectEvents();
+    _generateProblemsList();
+    _bindProblemSelectEvents();
+});
+
+function _loadDefaultProblemOnActiveTab() {
+    var selectedProblem = $('div.tab-pane.active li.active a').filter(":visible");
+    _loadProblemByLink(selectedProblem);
+}
+
+function _generateProblemsList() {
+    var problemType = _getOpenProblemType();
+    var indexPath = _getFullFilePath("index.json", problemType);
+    _loadJsonFile(indexPath, function(problemsList) {
+        var problemsContainer = $('div.tab-pane.active ul');
+        $.each(problemsList, function(index, problem) {
+            problemsContainer.append("<li><a href='#" + problem + "' data-toggle='tab'>" + problem + "</a></li>")
+        });
+        $('li', problemsContainer).first().addClass('active');
+        _loadDefaultProblemOnActiveTab();
+    })
+}
+
+function _bindProblemSetSelectEvents() {
+    $('#problemsets').on('shown', function() {
+        if (!$('div.tab-pane.active ul li').size()) {
+            _generateProblemsList();
+        }
+    });
+}
+
+function _bindProblemSelectEvents() {
+    $('#tsp_tab, #vrp_tab, #wlp_tab').on('click', 'a', function() {
+        _loadProblemByLink($(this))
+    });
+}
+
+function _loadProblemByLink(link) {
+    var fileName = _jsonFileNameFromLink(link);
+    var problemType = _getOpenProblemType();
+    var fullPath = _getFullFilePath(fileName, problemType);
+    _loadJsonFile(fullPath, _selectVisualizer(fileName));
+}
+
+function _getOpenProblemType() {
+    var problemTabId = $('div.tab-pane.active').attr('id');
+    return problemTabId.startsWith('tsp') ? "tsp"
+         : problemTabId.startsWith('vrp') ? "vrp"
+         : problemTabId.startsWith('wlp') ? "wlp"
+         : undefined;
+}
+
+function _jsonFileNameFromLink(link) {
+    return link.attr('href').substring(1) + ".json"
+}
+
+function _getFullFilePath(fileName, problemType) {
+    switch (problemType) {
+        case 'tsp': return ("data/tsp/" + fileName);
+        case 'vrp': return ("data/vrp/" + fileName);
+        case 'wlp': return ("data/wlp/" + fileName);
+        default: return undefined
+    }
+}
+
