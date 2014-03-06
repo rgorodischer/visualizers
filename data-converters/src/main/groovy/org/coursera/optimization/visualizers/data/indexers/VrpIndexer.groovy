5b23cc189bc43125de098bb96afd0e92f3009c33
Roman Gorodischer <Qwest@Romans-MacBook-Air-2.local>
2014-03-06 13:10:51 +0200

    Changed VRP index format: made description field more consize

--- data-converters/src/main/groovy/org/coursera/optimization/visualizers/data/indexers/VrpIndexer.groovy
+++ data-converters/src/main/groovy/org/coursera/optimization/visualizers/data/indexers/VrpIndexer.groovy
@@ -6,10 +6,10 @@ package org.coursera.optimization.visualizers.data.indexers
  * Resulting JSON has the following format:
  *
  * [
- *  { "fileName": converted_file_name, "description": "N1 points and V1 vehicles of capacity C1" },
- *  { "fileName": converted_file_name, "description": "N2 points and V2 vehicles of capacity C2" },
+ *  { "fileName": converted_file_name, "description": "N1 points, V1 vehicles" },
+ *  { "fileName": converted_file_name, "description": "N2 points, V2 vehicles" },
  *  .........
- *  { "fileName": converted_file_name, "description": "Nn points and Vn vehicles of capacity Cn" }
+ *  { "fileName": converted_file_name, "description": "Nn points, Vn vehicles" }
  * ]
  *
  * @author roman.gorodyshcher
@@ -19,6 +19,6 @@ class VrpIndexer extends Indexer {
     @Override
     String describe(problemModel) {
         //Points number is computed as customers number plus one warehouse
-        "${problemModel.customers.size() + 1} points and $problemModel.vehicles vehicles of capacity $problemModel.capacity"
+        "${problemModel.customers.size() + 1} points, $problemModel.vehicles vehicles"
     }
 }
